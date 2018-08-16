2775bf58f2b890bd90f9791c3391eba2d5851ea3
TheBayBen <41971958+TheBayBen@users.noreply.github.com>
2018-08-16 16:17:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -137,12 +137,12 @@ def makeWebhookResult(data, req):
             speech = "Tomorrow the weather in " + location['city'] + " will be " + forecast[int(estimation)]['text'] + \
                      " with " + forecast[int(estimation)]['high'] + " degrees for the maximum and " + forecast[int(estimation)]['low'] + \
                      " degrees for the minimum"
-        #elif dateDialogflow == forecast[int(estimation)]['date']:
-        #    speech = "On the " + forecast[int(estimation)]['date'] + " the weather in " + location['city'] + " will be " + forecast[int(estimation)]['text'] + \
-        #             " with " + forecast[int(estimation)]['high'] + " degrees for the maximum and " + forecast[int(estimation)]['low'] + \
-        #             " degrees for the minimum"
-        else:
-            speech = "I have not the weather forecast at the given date, please give me another date" + " %d" %int(estimation)
+        elif dateDialogflow == forecast[int(estimation)]['date']:
+            speech = "On the " + forecast[int(estimation)]['date'] + " the weather in " + location['city'] + " will be " + forecast[int(estimation)]['text'] + \
+                     " with " + forecast[int(estimation)]['high'] + " degrees for the maximum and " + forecast[int(estimation)]['low'] + \
+                     " degrees for the minimum"
+        elif int(estimation) >= 10 :
+            speech = "I have not the weather forecast at the given date, please give me another date"
     print("Response:")
     print(speech)
 
