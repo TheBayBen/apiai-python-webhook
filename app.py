4b78d900d97046eec96b498d8255ed39db9511eb
TheBayBen <41971958+TheBayBen@users.noreply.github.com>
2018-08-16 15:55:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ def makeWebhookResult(data, req):
             speech = "On the " + forecast[int(estimation)]['date'] + " the weather in " + location['city'] + " will be " + forecast[int(estimation)]['text'] + \
                      " with " + forecast[int(estimation)]['high'] + " degrees for the maximum and " + forecast[int(estimation)]['low'] + \
                      " degrees for the minimum"
-        else:
+        elif dateDialogflow != forecast[int(estimation)]['date']:
             speech = "I have not the weather forecast at the given date, please give me another date"
                     
         
